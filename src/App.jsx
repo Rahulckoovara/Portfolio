aba02e21c34c995f7e5b86ef6e22d624f3d0dcb6
Rahulckoovara <rahulckoovara1@gmail.com>
2023-11-21 16:10:51 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+
+import './App.css'
+import About from './components/about/About';
+import Experience from './components/experience/Experience';
+import Header from './components/header/Header'
+import Nav from './components/nav/nav';
+
+import Footer from './components/footer/Footer';
+import Services from './components/services/Services';
+import Portfolio from './components/portfolio/Portfolio';
+import Contacts from './components/contacts/Contacts';
+function App() {
+
+  return (
+    <>
+    <Header/>
+    <Nav/>
+    <About/>
+    <Experience/>
+    <Services/>
+    <Portfolio/>
+    <Contacts/>
+    <Footer/>
+    </>
+    
+  )
+}
+
+export default App
