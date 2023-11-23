3e11efffea816087d7ce2215474af7707564259f
Rahulckoovara <rahulckoovara1@gmail.com>
2023-11-23 17:52:31 +0530

    fix typo

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import About from './components/about/About';
 import Experience from './components/experience/Experience';
 import Header from './components/header/Header'
-import Nav from './components/nav/nav';
+import Nav from './components/nav/Nav';
 
 import Footer from './components/footer/Footer';
 import Portfolio from './components/portfolio/Portfolio';
