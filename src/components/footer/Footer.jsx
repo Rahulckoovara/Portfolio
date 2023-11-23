e4db97985710e5969dc813e39d5f32bd0b6783a1
Rahulckoovara <rahulckoovara1@gmail.com>
2023-11-23 17:59:50 +0530

    update

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -15,7 +15,7 @@ const Footer = () => {
         <li><a href='#'>Home</a></li>
         <li><a href='#about'>About</a></li>
         <li><a href='#experience'>Experience</a></li>
-        <li><a href='#portfoli'>Portfolio</a></li>
+        <li><a href='#portfolio'>Portfolio</a></li>
        <li><a href='#contacts'>Contacts</a></li>
         
       </ul>
